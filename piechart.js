b059e1732dd050cf1761a2d8b227ec79eebbdfed
Jerry Lin <jerrylin3321@gmail.com>
2020-08-13 12:49:33 -0700

    added pie chart, logo, and basic html to extension

---
+++ piechart.js
@@ -0,0 +1,29 @@
+google.charts.load("current", {packages:["corechart"]});
+google.charts.setOnLoadCallback(drawChart);
+function drawChart() {
+    var data = google.visualization.arrayToDataTable([
+        ['Task', 'Hours per Day'],
+        ['Loan Already Paid Off',  1000],
+        ['Value Unpaid',     1000],
+        ['Save Today',      50],
+        
+    ]);
+    var formatter = new google.visualization.NumberFormat({
+        prefix: '$'
+    });
+    formatter.format(data, 1);
+
+    var options = {
+        title: 'Loan Pizza',
+        pieSliceText: 'value',
+        pieHole: 0.4,
+        slices: {
+            0: { color: 'green' },
+            1: { color: 'red' },
+            2: { color: 'orange' }
+          }
+    };
+
+    var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+    chart.draw(data, options);
+}
