3d2a80521391014b339464e0b937f35b66f16b9e
BryantH24 <gbhou5@gmail.com>
2020-08-13 17:15:41 -0400

    Style changes and api call to server for balance

--- popup.js
+++ popup.js
@@ -1,9 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var checkPageButton = document.getElementById('checkPage');
-  checkPageButton.addEventListener('click', function() {
+  // var checkPageButton = document.getElementById('checkPage');
+  // checkPageButton.addEventListener('click', function() {
+  //
+  //   chrome.tabs.getSelected(null, function(tab) {
+  //     document.getElementById('sc-item-Cf0ffe267-d465-474a-a98b-93b0be1a45b1').innerHTML += 'Insert Form Here'
+  //   });
+  // }, false);
+  function callback() {
+      if(xhr.readyState === XMLHttpRequest.DONE) {
+          if (xhr.status === 200) {
+              result = xhr.responseText;
+              console.log(result);
+              result = "<b>" + result + "<b>"
+              var div = document.getElementById('bankAccount');
+              div.innerHTML += result;
+          }
+      }
+  };
 
-    chrome.tabs.getSelected(null, function(tab) {
-      document.getElementById('sc-item-Cf0ffe267-d465-474a-a98b-93b0be1a45b1').innerHTML += 'Insert Form Here'
-    });
-  }, false);
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "https://harmonize-api-endpoint.herokuapp.com/", true);
+  xhr.onreadystatechange = callback;
+  xhr.send();
 }, false);
