7eadd28aa391fdf89f3d729b49f0cd6c981716c4
cqthyy <ccao35@wisc.edu>
2020-08-14 01:00:08 -0400

    update skip/done buttons

--- content-script.js
+++ content-script.js
@@ -1,13 +1,16 @@
-const body = document.getElementsByClassName('a-size-medium a-color-base sc-price sc-white-space-nowrap sc-product-price a-text-bold')[0]
 
-const buttons = document.createElement('div');
-buttons.className = "mucho";
-buttons.id = "gracias";
-buttons.innerHTML = '<button type="button home-button" id="button1" >Want</button><button type="button contact-button" id="button2">Need</button>';
-body.append(buttons);
+const skipbutton = document.getElementsByClassName('a-box a-color-alternate-background sc-buy-box-inner-box')[0];
+for (var i = 0; i < 10000; i++) {
+const body = document.getElementsByClassName('a-size-medium a-color-base sc-price sc-white-space-nowrap sc-product-price a-text-bold')[i];
+  const buttons = document.createElement('div');
+  buttons.className = "mucho";
+  buttons.id = "gracias";
+  buttons.innerHTML = '<button type="button home-button" id="button1" >Want</button><button type="button contact-button" id="button2">Need</button>';
+  body.append(buttons);
+}
 
-const ayy = document.createElement('div');
-ayy.className = 'ayy';
-ayy.id = 'ayy';
-ayy.innerHTML = '<body>hulllawathav</body>';
-body.append(ayy);
+const skip = document.createElement('div');
+skip.className = "button";
+skip.id = "skipdone";
+skip.innerHTML = '<button type="button home-button" id="button3" >Skip</button><button type="button home-button" id="button4">Done</button>';
+skipbutton.append(skip);
